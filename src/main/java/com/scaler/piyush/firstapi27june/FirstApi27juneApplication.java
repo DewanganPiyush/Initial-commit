b91d1881b195a385a8d7477a905c652c1522b908
DewanganPiyush <dewanganpiyush999@gmail.com>
2024-07-02 19:32:19 +0530

    Initial commit

---
+++ src/main/java/com/scaler/piyush/firstapi27june/FirstApi27juneApplication.java
@@ -0,0 +1,16 @@
+package com.scaler.piyush.firstapi27june;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApi27juneApplication {
+
+    public static void main(String[] args) {
+
+
+
+        SpringApplication.run(FirstApi27juneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/piyush/firstapi27june/FirstApi27juneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.piyush.firstapi27june;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApi27juneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
